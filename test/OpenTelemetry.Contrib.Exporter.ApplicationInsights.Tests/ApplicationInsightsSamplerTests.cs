f7df57fb1d239438cbc17252ca7ae13861ffc648
Austin Tan <austinwtan@gmail.com>
2021-07-27 00:01:09 -0700

    Restart branch

--- test/OpenTelemetry.Contrib.Exporter.ApplicationInsights.Tests/ApplicationInsightsSamplerTests.cs
+++ test/OpenTelemetry.Contrib.Exporter.ApplicationInsights.Tests/ApplicationInsightsSamplerTests.cs
@@ -1,17 +1,33 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+﻿// <copyright file="ApplicationInsightsSamplerTests.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
 
 using System;
 using System.Diagnostics;
 
+using OpenTelemetry.Contrib.Exporter.ApplicationInsights;
 using OpenTelemetry.Trace;
-
 using Xunit;
 
-namespace OpenTelemetry.Contrib.Exporter.Tests {
-    public class ApplicationInsightsSamplerTests {
+namespace OpenTelemetry.Contrib.Exporter.ApplicationInsights.Tests
+{
+    public class ApplicationInsightsSamplerTests
+    {
         [Fact]
-        public void VerifyHashAlgorithmCorrectness() {
+        public void VerifyHashAlgorithmCorrectness()
+        {
             byte[] testBytes = new byte[]
             {
                           0x8F, 0xFF, 0xFF, 0xFF,
@@ -19,20 +35,20 @@ namespace OpenTelemetry.Contrib.Exporter.Tests {
                           0, 0, 0, 0, 0, 0, 0, 0,
             };
             byte[] testBytes2 = new byte[]
-{
+            {
                           0x0F, 0x1F, 0x2F, 0x3F,
                           0x4F, 0x5F, 0x6F, 0x7F,
                           0x8F, 0x9F, 0xAF, 0xBF,
                           0xCF, 0xDF, 0xEF, 0xFF,
-};
+            };
             ActivityTraceId testId = ActivityTraceId.CreateFromBytes(testBytes);
             ActivityTraceId testId2 = ActivityTraceId.CreateFromBytes(testBytes2);
 
-            ActivityContext parentContext = new ActivityContext();
+            ActivityContext parentContext = default(ActivityContext);
             SamplingParameters testParams = new SamplingParameters(parentContext, testId, "TestActivity", ActivityKind.Internal);
             SamplingParameters testParams2 = new SamplingParameters(parentContext, testId2, "TestActivity", ActivityKind.Internal);
 
-            ApplicationInsightsSampler zeroSampler = new ApplicationInsightsSampler(0);
+            var zeroSampler = new ApplicationInsightsSampler(0);
             ApplicationInsightsSampler oneSampler = new ApplicationInsightsSampler(1);
 
             // 0.86 is below the sample score for testId1, but strict enough to drop testId2
